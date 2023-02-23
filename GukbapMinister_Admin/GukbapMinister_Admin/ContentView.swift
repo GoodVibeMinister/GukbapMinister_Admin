7270e0f9c915a2a2708a767d4032a9810a586930
MartinLeeSJ <tjr2922@gmail.com>
2023-02-23 11:19:21 +0900

    [Add] Add Project

---
+++ GukbapMinister_Admin/GukbapMinister_Admin/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GukbapMinister_Admin
+//
+//  Created by Martin on 2023/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GukbapMinister_Admin/GukbapMinister_Admin/GukbapMinister_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  GukbapMinister_AdminApp.swift
+//  GukbapMinister_Admin
+//
+//  Created by Martin on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GukbapMinister_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
